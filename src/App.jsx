b87bdfa8c02264107fa4e37665db0baf083d8a11
Mariangelica Rodriguez <gromarant@gmail.com>
2023-07-08 15:21:47 +0200

    Add context and cotext provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
+import { pokemonListContext } from './context/pokemonListContext';
 import { NextUIProvider } from '@nextui-org/react';
 import { BrowserRouter } from 'react-router-dom';
+import { useState } from 'react';
 
 const App = () => {
+  const [pokemonList, setPokemonList] = useState([])
 
   return (
     <>
-    <NextUIProvider>
-      <BrowserRouter>
-        <Header />
-        <Main />
-      </BrowserRouter>
-      <Footer />
-    </NextUIProvider>
+      <pokemonListContext.Provider value={{pokemonList, setPokemonList}}>
+        <NextUIProvider>
+          <BrowserRouter>
+            <Header />
+            <Main />
+          </BrowserRouter>
+          <Footer />
+        </NextUIProvider>
+      </pokemonListContext.Provider> 
     </>
   )
 }
