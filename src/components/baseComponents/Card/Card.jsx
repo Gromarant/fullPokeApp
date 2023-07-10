2e6f269e3a80d32415eb3347a81a8f3a6fb242bc
Mariangelica Rodriguez <gromarant@gmail.com>
2023-07-10 23:56:10 +0200

    Update styles

--- src/components/baseComponents/Card/Card.jsx
+++ src/components/baseComponents/Card/Card.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Card as NextUiCard, Grid, Row } from "@nextui-org/react";
+import { Card as NextUiCard, Row } from "@nextui-org/react";
 import { Link } from 'react-router-dom';
 
 const Card = ({id, name, image}) => {
@@ -7,30 +7,22 @@ const Card = ({id, name, image}) => {
 
   return (
     <>
-      {/*<Grid.Container > gap={2} justify="flex-start"
-          <Grid xs={6} sm={3}>*/}
-            <NextUiCard isPressable>
-              <Link to={`/pokemon/${id}`}>
-                <NextUiCard.Body >{/*css={{ p: 0 }}*/}
-                  <NextUiCard.Image
-                    src={image}
-                    objectFit="cover"
-                    width="fit-content"
-                    alt={name}
-                  />{/*objectFit="cover"
-                    width="fit-content"*/}
-                </NextUiCard.Body>
-              </Link>
-              <NextUiCard.Footer >{/*css={{ justifyItems: "flex-start" }}*/}
-                <Row>{/* wrap="wrap" justify="space-between" align="center" color=''*/}
-                <Link
-                  to={`/pokemon/${id}`}
-                >{name}</Link>
-                </Row>
-              </NextUiCard.Footer>
-            </NextUiCard>
-          {/* </Grid>
-      </Grid.Container> */}
+      <NextUiCard isPressable>
+        <Link to={`/pokemon/${id}`}>
+          <NextUiCard.Body >
+            <NextUiCard.Image
+              src={image}
+              alt={name}/>
+          </NextUiCard.Body>
+        </Link>
+        <NextUiCard.Footer >
+          <Row>
+          <Link
+            to={`/pokemon/${id}`}
+          >{name}</Link>
+          </Row>
+        </NextUiCard.Footer>
+      </NextUiCard>
   </>
   );
 };
