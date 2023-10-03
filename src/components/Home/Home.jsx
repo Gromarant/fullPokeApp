5d447c84b6e1f739172b5c37a6265422c3b02d64
Mariangelica Rodriguez <gromarant@gmail.com>
2023-10-03 14:04:22 +0200

    get/index of each card to change the pokemon

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,31 +1,50 @@
-import {pokemons} from '../Hero/Search/SearchData.js';
 import { v4 as uuidv4 } from 'uuid';
+import {pokemons} from '../Hero/Search/SearchData.js';
 import CardHome from '../baseComponents/CardHome/CardHome.jsx';
 
 const Home = () => {
   let showList = [];
+  const pokeList = pokemons.results;
+  let newPokemon = null;
+  
+  const randomPokemon = () => pokeList[Math.floor(Math.random() * 10264)];
   
   const pokemonsToShow = () => {
-    const pokeList = pokemons.results;
 
     if (pokeList.length > 0) {
       while (showList.length < 20) {
-        let newPokemon = pokeList[Math.floor(Math.random() * 10264)];
-          if (newPokemon) {
-            showList.push(newPokemon);
-          }
+        newPokemon = randomPokemon();
+
+        if (newPokemon) {
+          showList = [...showList, newPokemon];
+        }
+        newPokemon = null;
       }
     }
-    console.log('showList_id', showList);
   };
   pokemonsToShow();
 
+  
+  const handleClick  = (i) => {
+    console.log(`clicked pokemon at index ${i}`);
+    while (newPokemon === null || newPokemon === undefined) {
+      newPokemon = randomPokemon();
+
+      if (newPokemon) {
+        console.log(newPokemon);
+        showList.map((pokemon, index) => index === i ? pokemon[i] = newPokemon : pokemon);
+      }
+    }
+    newPokemon = null;
+  };
+  
   return (
     <>
       <h1>Pokémons</h1>
+      <p className='desc'>App description here!</p>
       <section className='homeCards_section'>
         {
-          showList?.map(pokemon => <CardHome image={pokemon.image} name={pokemon.name} key={uuidv4()}/>)
+          showList?.map((pokemon, index) => <CardHome handleClickEvent={() => handleClick(index)} image={pokemon.image} name={pokemon.name} title={index} key={uuidv4()}/>)
         }
       </section>
     </>
--- src/components/baseComponents/CardHome/CardHome.jsx
+++ src/components/baseComponents/CardHome/CardHome.jsx
@@ -2,30 +2,33 @@ import PropTypes from 'prop-types';
 import './CardHome.css';
 import { Card, Col, Text } from "@nextui-org/react";
 
-const CardHome = ({image, name}) => {
+const CardHome = ({image, name, handleClickEvent}) => {
 
   return (
-    <Card className='cardHome'>
-      <Card.Header>
-        <Col>
-          <Text size={18} weight="bold" transform="uppercase">
-            {name}
-          </Text>
-        </Col>
-      </Card.Header>
-      <Card.Image
-        src={image}
-        objectFit="cover"
-        width="fit-content"
-        alt={name}
-      />
-    </Card>
+    <article onClick={handleClickEvent}>
+      <Card className='cardHome'>
+        <Card.Header>
+          <Col>
+            <Text size={18} weight="bold" transform="uppercase">
+              {name}
+            </Text>
+          </Col>
+        </Card.Header>
+        <Card.Image
+          src={image}
+          objectFit="cover"
+          width="fit-content"
+          alt={name}
+        />
+      </Card>
+    </article>
   );
 };
 
 CardHome.propTypes = {
   image: PropTypes.string,
   name: PropTypes.string,
+  handleClickEvent: PropTypes.func,
 }
 
 
