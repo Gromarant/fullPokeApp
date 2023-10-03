e63bf85a1080d46c3a7157b65f13aad3f320644a
Mariangelica Rodriguez <gromarant@gmail.com>
2023-10-03 20:06:14 +0200

    Add/search btn with styles in to the homePage

--- src/components/baseComponents/ButtonAct/ButtonAct.jsx
+++ src/components/baseComponents/ButtonAct/ButtonAct.jsx
@@ -1,19 +1,15 @@
 import PropTypes from 'prop-types';
 import { Button } from "@nextui-org/react";
 
-const ButtonAct = ({
-  color="gradient",
-  value,
-}) => {
+const ButtonAct = ({value}) => {
   return (
-    <Button className='submit_btn' color={color} auto type='submit'>
+    <Button className='submit_btn searchBtn' type='submit'>
       {value}
     </Button>
   );
 };
 
 ButtonAct.propTypes = {
-  color: PropTypes.string,
   value: PropTypes.string,
   event: PropTypes.func,
 }
