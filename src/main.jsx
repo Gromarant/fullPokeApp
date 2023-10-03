4a2ff8927472b304179aadfc646c1903d52e01ee
Mariangelica Rodriguez <gromarant@gmail.com>
2023-10-03 18:05:12 +0200

    Update/styles with @forward and @use from  SASS

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../src/styles/styles.scss';
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
