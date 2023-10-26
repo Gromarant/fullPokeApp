caf37284c667b91e17066face09100d45959e146
Mariangelica Rodriguez <gromarant@gmail.com>
2023-10-26 19:58:51 +0200

    add/error alerts into form of pokemon creatio

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,6 @@
-import { useForm } from "react-hook-form";
 import { useState, useContext } from 'react';
+import Swal from 'sweetalert2';
+import { useForm } from 'react-hook-form';
 import {useNavigate} from 'react-router-dom';
 import { pokemonCreatedListContext } from '../context/pokemonCreatedListContext';
 import { createdPokemonContext } from '../context/createdPokemonContext';
@@ -25,6 +26,17 @@ const Create = () => {
   } =useForm({ defaultValues });
   const navigate = useNavigate();
 
+  console.log('errors', errors);
+
+  const handleErrors = (error, title, message) => {
+    if (error) {
+      Swal.fire({
+        icon: 'error',
+        titleText: title,
+        html: message,
+      })
+    }
+  };
 
   const handleChange = (e) => {
 
@@ -47,79 +59,95 @@ const Create = () => {
           setInputValue(defaultValues)
         })}>
           <h3 className='form__title'>New Pokémon data</h3>
-          <label htmlFor="owner_id" className='form_label'>Id
+          <label htmlFor='owner_id' className='form_label'>Id *
           <input type='string' name='owner_id' id='owner_id' autoComplete='off' className='form_input' {...register('owner_id', {
             required: 'ID field is required',
-            minLength: {
-              value: 1,
-              message: "must have at least 1 character"
-            },
-            maxLength:{
-              value: 20,
-              message: "must have a maximum of 20 characters"
-            },
-          })} onChange={handleChange} placeholder='pokemon id' value={inputValue.owner_id}/></label>
-          {errors.owner_id?.type === "required" && (
-          <p role="alert">{errors.owner_id?.message}</p>)}
+            pattern: /([A-Z](?<=[A-Z])-\d{3,6})/,
+            minLength: 5,
+            maxLength: 8,
+          })} onChange={handleChange} placeholder='X-123... or X-123456' value={inputValue.owner_id}/></label>
+          {errors.owner_id &&  
+            handleErrors(
+              errors.owner_id,
+              'invalid Id:',
+              'Must begin with:' +
+              '<b> 1 Capital letter</b> + (-) + <b>3-6 numbers.</b>' +
+              ' EXAMPLE:  <b>X-157961</b>' +
+              ' Length:  <b>5-8 characters</b>',
+            )
+          }
 
-          <label htmlFor="name" className='form_label'>Name
+          <label htmlFor='name' className='form_label'>Name *
           <input type='string' name='name' id='name' autoComplete='off' className='form_input' {...register('name', {
             required: 'Name field is required',
-            minLength: {
-              value: 3,
-              message: "must have at least 3 characters"
-            },
-            maxLength:{
-              value: 20,
-              message: "must have a maximum of 20 characters"
-            },
+            pattern: /^[A-Za-z]{1,20}?$|([A-Za-z]{1,20}(?<=[A-Za-z])-\d{4})/ig,
+            minLength: 3,
+            maxLength:20,
           })}
-          onChange={handleChange} placeholder='pokemon name' value={inputValue.name}/></label>
-          {errors.name?.type === "required" && (
-          <p role="alert">{errors.name?.message}</p>)}
+          onChange={handleChange} placeholder='Pokechi or pokechi-0001' value={inputValue.name}/></label>
+          {errors.name &&  
+            handleErrors(
+              errors.name,
+              'invalid Name:',
+              'Must start with:' +
+              '<b> Letters</b> + optional : (-) + <b>4 numbers.</b>' +
+              ' EXAMPLE:  <b>Pokeboy</b> or <b>Pokeboy-0025</b>' +
+              ' Length:  <b>3-20 characters</b>',
+            )
+          }
 
-          <label htmlFor="image" className='form_label'>Url image
+          <label htmlFor='image' className='form_label'>Url image *
           <input type='url' name='image' id='image' className='form_input' {...register('image', {
-             required: 'Url image field is required',
-            maxLength: {
-              value: 200,
-              message: "must have a maximum of 50 characters"
-            }
+            required: 'Url image field is required',
+            pattern: /(https?:\/\/.*\.*)/i,
+            maxLength: 200,
           })}
           onChange={handleChange} placeholder='pokemon image' value={inputValue.image}/></label>
-          {errors.image?.type === "required" && (
-          <p role="alert">{errors.image?.message}</p>)}
+          {errors.image &&  
+            handleErrors(
+              errors.image,
+              'invalid Image url:',
+              'Must start with:' +
+              '<b> https</b>' +
+              ' EXAMPLE:  <b>https://images...</b>' +
+              ' Length:  <b> max 200 characters</b>',
+            )
+          }
 
-          <label htmlFor="typeOne" className='form_label'>TypeOne
+          <label htmlFor='typeOne' className='form_label'>TypeOne *
           <input type='string' name='typeOne' id='typeOne' autoComplete='off' className='form_input' {...register('typeOne', {
             required: 'TypeOne field is required',
-            minLength: {
-              value: 3,
-              message: "must have at least 3 characters"
-            },
-            maxLength:{
-              value: 20,
-              message: "must have a maximum of 20 characters"
-            },
+            pattern: /^[A-Za-z]{3,15}$/,
+            minLength: 3,
+            maxLength: 20,
           })}
-          onChange={handleChange} placeholder='pokemon typeOne' value={inputValue.typeOne}/></label>
-          {errors.typeOne?.type === "required" && (
-          <p role="alert">{errors.typeOne?.message}</p>)}
+          onChange={handleChange} placeholder='fire or water or ...' value={inputValue.typeOne}/></label>
+          {errors.typeOne &&  
+            handleErrors(
+              errors.typeOne,
+              'invalid TypeOne:',
+              'Must have only letters' +
+              ' EXAMPLE:  <b>fire</b> | <b>water</b> | <b>onomatopoeia</b>' +
+              ' Length:  <b>3-20 characters</b>',
+            )
+          }
 
-          <label htmlFor="typeTwo" className='form_label'>TypeTwo
+          <label htmlFor='typeTwo' className='form_label'>TypeTwo
           <input type='string' name='typeTwo' id='typeTwo' autoComplete='off' className='form_input' {...register('typeTwo', {
-            minLength: {
-              value: 5,
-              message: "must have at least 5 characters"
-            },
-            maxLength:{
-              value: 20,
-              message: "must have a maximum of 20 characters"
-            },
+            pattern: /^[A-Za-z]{3,15}$/,
+            minLength: 5,
+            maxLength:20,
           })}
           onChange={handleChange} placeholder='pokemon typeTwo' value={inputValue.typeTwo}/></label>
-          {errors.typeTwo?.type === "required" && (
-          <p role="alert">{errors.typeTwo?.message}</p>)}
+          {errors.typeTwo &&  
+            handleErrors(
+              errors.typeTwo,
+              'invalid TypeTwo:',
+              'Must have only letters' +
+              ' EXAMPLE:  <b>fairy"</b> | <b>poison</b> | <b>fighting</b>' +
+              ' Length:  <b>3-20 characters</b>',
+            )
+          }
 
           <ButtonAct value='Create'/>
         </form>
