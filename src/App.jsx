abc39ed5f3b35810fad81e10096f94fa1082a48d
Mariangelica Rodriguez <gromarant@gmail.com>
2024-04-02 17:42:28 +0200

    change/NavBar component, structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { createTheme, NextUIProvider } from "@nextui-org/react"
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/commonComponents/Header';
 import Main from './components/commonComponents/Main';
@@ -31,32 +30,21 @@ const App = () => {
   const addToListOfCreatedPokemons = (pokemon) => {
     itContainsThisPokemon(pokemon, listOfPokemonsCreated) ? null : setListOfPokemonsCreated([...listOfPokemonsCreated, pokemon]);
   };
-
-  const theme = createTheme({
-    type: "dark",
-    theme: {
-      colors: {
-        primary: '#2359be',
-      }
-    }
-  })
   
   return (
     <>
-      <NextUIProvider theme={theme}>
-        <pokemonContext.Provider value={{
-          listOfPokemons, setListOfPokemons, 
-          listOfPokemonsCreated, setListOfPokemonsCreated, 
-          createdPokemon, setCreatedPokemon, 
-          searchedPokemon, setSearchedPokemon, 
-          addToListOfPokemons, addToListOfCreatedPokemons
-        }}>
-          <BrowserRouter>
-            <Header />
-            <Main />
-          </BrowserRouter>
-        </pokemonContext.Provider>
-      </NextUIProvider>
+      <pokemonContext.Provider value={{
+        listOfPokemons, setListOfPokemons, 
+        listOfPokemonsCreated, setListOfPokemonsCreated, 
+        createdPokemon, setCreatedPokemon, 
+        searchedPokemon, setSearchedPokemon, 
+        addToListOfPokemons, addToListOfCreatedPokemons
+      }}>
+        <BrowserRouter>
+          <Header />
+          <Main />
+        </BrowserRouter>
+      </pokemonContext.Provider>
     </>
   )
 }
